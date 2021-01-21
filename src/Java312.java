ccdeb17c5acc052c3f456b6d6765f01c8ced82a8
SoloVoice <a.v.rustamov@gmail.com>
2021-01-22 00:53:45 +0300

    homework

--- src/Java312.java
+++ src/Java312.java
@@ -1,2 +1,19 @@
-public class Java312 {
+import java.util.ArrayList;
+
+public class java312 {
+    public static void main(String[] args) {
+        String[] arr = new String[] {"один", "два", "три", "четыре", "пять"};
+        ArrayList arrList = arrayTransformToArrList(arr);
+        for (Object i: arrList) {
+            System.out.println(i);
+        }
+    }
+
+    public static ArrayList arrayTransformToArrList(Object[] arr) {
+        ArrayList arrList = new ArrayList();
+        for (Object o: arr) {
+            arrList.add(o);
+        }
+        return arrList;
+    }
 }
